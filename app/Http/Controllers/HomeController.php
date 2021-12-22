0807193de660dbb0bde874980f2006454668ca79
GiovanniConti <giovanni.conti@live.it>
2021-12-22 19:23:27 +0100

    Created MVC and connected to the database

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Movie;
+
+class HomeController extends Controller {
+  function index() {
+    $movies = Movie::all();
+    dump($movies);
+
+    return view('home.index');
+  }
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.default')
+
+@section('page_title', "Homepage Movies")
+
+@section('content')
+    <h1>Home Page</h1>
+@endsection
